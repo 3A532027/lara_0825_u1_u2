c03e8acdec6903613aa3b27c33c1ca6dcc649304
3A532027 <s3a532027@student.nuct.edu.tw>
2019-01-20 03:46:21 +0800

    練習二

--- app/post.php
+++ app/post.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class post extends Model
 {
-    //
+    protected $table= 'posts';
+
+    protected $fillable=[
+        'title',
+        'content',
+        'is_feature'
+    ];
+
 }
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,15 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  //  return view('welcome');
+    //    \App\Post::create([
+//        'title'=>'test title',
+//        'content'=>'test content',
+//    ]);
+
+    $post =new\App\Post();
+    $post ->title = 'test title';
+    $post ->content = 'test content';
+    $post ->save();
+
 });
