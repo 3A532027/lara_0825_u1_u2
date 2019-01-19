90e4a2838f8e75159c511555f9acc35832c188b1
3A532027 <s3a532027@student.nuct.edu.tw>
2019-01-20 04:19:46 +0800

    調整seeder

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use App\Post;
+use Faker\Factory as Faker;
+use Carbon\Carbon;
 
 
 class PostsTableSeeder extends Seeder
@@ -14,10 +16,23 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        Post::truncate();
+
         foreach(range(1,20)as$number){
+
+            $total=20;
+            $faker=Faker::create('zh_TW');
             Post::create([
                 'title'=>'title'.$number,
                 'content'=>'content'.$number,
+//                'title'=>'title'.$number,
+//                'content'=>'content'.$number,
+
+                'title'=>$faker->sentence,
+                'content'=>$faker->paragraph,
+                'is_feature'=>rand(0,1),
+                'created_at'=>Carbon::now()->subDays($total-$number),
+                'updated_at'=>Carbon::now()->subDays($total-$number),
             ]);
         }
     }
